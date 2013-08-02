ab7149e928cc2d4fbca6e69c2e5edffed5a7bd8d
Shawn Crosby <scrosby@salesforce.com>
2013-08-02 11:58:11 -0300

    Added ability to create a link to code collaborator in GUS as well as be able to update an existing review @updates W-1774931 @update_review 510272

--- collab/CodeCollab.py
+++ collab/CodeCollab.py
@@ -2,8 +2,11 @@ import subprocess, re, tempfile
 from getpass import getpass
 
 class CodeCollabClient:
+    def __init__(self, server='https://code-collab.soma.salesforce.com'):
+        self.server=server
+        
     def create_collab(self, title, overview):
-        out = subprocess.check_output(['ccollab', '--no-browser','admin', 'review', 'create', '--title', title, '--custom-field' , 'Overview="%s"' % overview])
+        out = subprocess.check_output(['ccollab', '--no-browser','admin', 'review', 'create', '--title', title, '--custom-field' , 'Overview=%s\n%s' % (title, overview)])
         review_regex = re.compile(r"Review #(\d*):")
         regex_id = review_regex.search(out).group(1)
         return regex_id
@@ -12,7 +15,7 @@ class CodeCollabClient:
         f = tempfile.NamedTemporaryFile(delete=False)
         f.write(diff)
         f.close()
-        subprocess.check_output(['ccollab', '--no-browser', 'adddiffs', '--upload-comment', comment, reviewid, f.name])
+        subprocess.call(['ccollab', '--no-browser', 'adddiffs', '--upload-comment', comment, reviewid, f.name])
 
     def add_reviewers(self, reviewid, author, reviewers):
         command = ['ccollab','--no-browser','admin','review']
@@ -29,19 +32,46 @@ class CodeCollabClient:
             for reviewer in reviewlist:
                 command.append('--participant')
                 command.append('reviewer=%s' % reviewer)
-        subprocess.check_output(command)
+        subprocess.call(command)
 
     def get_current_user(self):
         command = ['ccollab', '--scm', 'none', 'info']
-        data = subprocess.check_output(command)
-        regex = re.compile("Connected as:.*\((.*)\)", re.MULTILINE)
-        user = regex.search(data).group(1)
+        try:
+            data = subprocess.check_output(command)
+            regex = re.compile("Connected as:.*\((.*)\)", re.MULTILINE)
+            user = regex.search(data).group(1)
+        except:
+            user = None
+            
         return user
+    
+    def get_review_title(self, reviewid):
+        return self.get_review_data(reviewid, xpath='//reviews/review/general/title/text()')
+    
+    def get_review_link(self, reviewid):
+        return "%s/go?page=ReviewDisplay&reviewid=%s" % (self.server, reviewid)
+    
+    def get_last_review_id(self):
+        return self.get_review_data('last', xpath='string(//reviews/review/@reviewId)')
+    
+    def get_review_status(self, reviewid):
+        return self.get_review_data(reviewid, xpath='//reviews/review/general/phase/text()')
+    
+    def get_review_author(self, reviewid):
+        return self.get_review_data(reviewid, xpath='//reviews/review/participants/participant/[role="Author"]/../login/text()')
+        
+    def get_review_data(self, reviewid, xpath=None):
+        command = ['ccollab', 'admin', 'review-xml', reviewid]
+        if xpath is not None:
+            command.append('--xpath')
+            command.append(filter)
+        out = subprocess.check_output(command)
+        return out
     
     def login(self):
         user = raw_input('Enter your Code Collab username: ')
         passwd = getpass.getpass('Enter your Code Collab password: ')
-        command = ['ccollab', 'login', 'https://code-collab.soma.salesforce.com', user, passwd]
+        command = ['ccollab', 'login', self.server, user, passwd]
         subprocess.call(command)
     
     def done(self, reviewid):
